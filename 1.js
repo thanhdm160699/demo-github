c9564b9279b77e03ee25bf490d92ccc95c3a56b2
Đặng  Minh Thành <thanhmmo1999@gmail.com>
2020-05-02 12:24:30 +0700

    Add 1.css, 1.html, 1.js

---
+++ 1.js
@@ -0,0 +1,46 @@
+// console.log(    'Hello!!!');
+// // biến là một khái niệm dùng để lưu trữ dữ liệu
+// var bien1 = 10;
+// console.log(bien1);
+
+// Toán tử
+// var x=10, y=2;
+// var z="hello ";
+// console.log(x+y);
+// console.log(z+x);
+
+// chaythu = function(){
+//     x= x-1;
+//     console.log("gia tri cua b la: "+x);
+// }
+
+// So sanh
+// console.log();
+//------------------------------------------------------------//
+//Mang
+/*
+    Mảng lưu được vô số giá trị 
+*/
+
+
+// var hv1 = "tai";
+// var hv2 ="Dong";
+// var hv3 = "Tien";
+
+// // Cach khai bao Mang 1
+// var hv = ["tai", "DOng", "Tien"];
+// console.log(hv2);
+
+// // Cach khai bao Mang 2
+// var hv2 = [];
+// hv2[0] = "Luu";
+// hv2[1] = "Ten";
+// console.log(hv2[0]);
+
+// // Cach 3
+// var hv3 = new Array("kiet","Ling", "Tien");
+// console.log(hv3[1]);
+
+//-------------------------------------------------------------
+
+
